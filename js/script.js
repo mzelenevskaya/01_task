0fa9b0ed1f96d6f24089fa45b6d3d5404eeb1fc3
mzelenevskaya <mzelenevskaya@gmail.com>
2022-06-07 20:59:47 +0300

    feat: change html and css layouts for 01_task

--- js/script.js
+++ js/script.js
@@ -31,3 +31,40 @@ function switchTheme(e) {
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+// modal window
+const myBtn = document.querySelector('#myButton');
+const closeBtn = document.querySelector('.promo-content__modal-close');
+const modal = document.querySelector('.promo-content__modal');
+
+function toggleModal(){
+    if (modal.style.display === 'block') {
+        modal.style.display = 'none';
+    } else {
+        modal.style.display = 'block';
+    }    
+}
+
+myBtn.addEventListener('click', toggleModal);
+closeBtn.addEventListener('click', toggleModal);
+
+
+// modal window
+// var modal = document.getElementById('myModal');
+// var button = document.getElementById("myButton");
+// var span = document.getElementsByClassName("promo-content__modal-close")[0];
+
+// button.onclick = function () {
+//     modal.style.display = "block";    
+// }
+
+// span.onclick = function () {
+//     modal.style.display = "none";    
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
